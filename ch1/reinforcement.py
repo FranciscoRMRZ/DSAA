fd46f910248830994a158d24ee1468f507aef830
Francisco Ramírez <py.francisco.ramirez@gmail.com>
2021-03-04 19:57:38 -0600

    Solved exercises 1.1 and 1.2

--- ch1/reinforcement.py
+++ ch1/reinforcement.py
@@ -13,5 +13,20 @@ def is_multiple(n, m):
         i += 1
     return False
 
+# R-1.2 Write a short Python function, is even(k), that 
+# takes an integer value and returns True if k is even, 
+# and False otherwise. However, your function cannot use 
+# the multiplication, modulo, or division operators.
+
+def is_even(k):
+    while k >= 0:
+        if k == 0:
+            return True
+        else:
+            k -= 2
+    return False
+
+
 if __name__ == '__main__':
-    print(is_multiple(1_000_001,5))
+    # print(is_multiple(1_000_001,5))
+    print(list(map(is_even, range(10))))
