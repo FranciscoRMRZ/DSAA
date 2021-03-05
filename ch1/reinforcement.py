31cee2e12b41f9068a7970625b8afd5429b5d137
Francisco Ramírez <py.francisco.ramirez@gmail.com>
2021-03-04 19:37:11 -0600

    Initializing repo for Data structures and algorithms

---
+++ ch1/reinforcement.py
@@ -0,0 +1,17 @@
+# Reinforcement
+#
+# R-1.1 Write a short Python function, is multiple(n, m), 
+# that takes two integer values and returns True if n is 
+# a multiple of m, that is, n = mi for some integer i, 
+# and False otherwise.
+
+def is_multiple(n, m):
+    i = 0
+    while i < n:
+        if (i * m <= n) and (i * m == n):
+            return True
+        i += 1
+    return False
+
+if __name__ == '__main__':
+    print(is_multiple(1_000_001,5))
