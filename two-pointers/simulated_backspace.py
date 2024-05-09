53ef9022b79ba83179754c384e15304cc4cc8c31
Francisco Ramirez <py.francisco.ramirez@gmail.com>
2024-05-08 20:42:30 -0600

    add simulated backspace comparison problem

---
+++ two-pointers/simulated_backspace.py
@@ -0,0 +1,42 @@
+# Given two strings containing backspaces (identified by the character ‘#’),
+# check if the two strings are equal
+
+
+def remove_backpaces(word: str) -> str:
+    stack = []
+    for char in word:
+        if char != "#":
+            stack.append(char)
+        else:
+            stack.pop()
+
+    return "".join(stack)
+
+
+def compare_strings(str1: str, str2: str) -> bool:
+    return remove_backpaces(str1) == remove_backpaces(str2)
+
+
+if __name__ == "__main__":
+    sample_1 = "xy#z"
+    sample_2 = "xzz#"
+    sample_3 = "xywrrmp"
+    sample_4 = "xywrrmu#p"
+
+    print("Remove backspaces from sample_1:", sample_1)
+    print("Result:", remove_backpaces(sample_1))
+    print("Remove backspaces from sample_2:", sample_2)
+    print("Result:", remove_backpaces(sample_2))
+    print(
+        "Are sample_1 and sample_2 equal after backspace removal?:",
+        compare_strings(sample_1, sample_2),
+    )
+
+    print("Remove backspaces from sample_3:", sample_3)
+    print("Result:", remove_backpaces(sample_3))
+    print("Remove backspaces from sample_4:", sample_4)
+    print("Result:", remove_backpaces(sample_4))
+    print(
+        "Are sample_1 and sample_2 equal after backspace removal?:",
+        compare_strings(sample_3, sample_4),
+    )
