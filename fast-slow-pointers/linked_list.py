76db67cfca5e4b0c27851abd8dac9fcf1e51671a
Francisco Ramirez <py.francisco.ramirez@gmail.com>
2024-05-18 12:24:52 -0600

    change has_cycle to return the two pointers meeting point

--- fast-slow-pointers/linked_list.py
+++ fast-slow-pointers/linked_list.py
@@ -7,14 +7,21 @@ class Node:
 def has_cycle(head: Node) -> bool:
     if not head or not head.next:
         return False
+        return None
 
     slow = head
     fast = head.next
+    fast = head
 
     while fast != slow:
         if not fast or not fast.next:
             return False
+    while fast and fast.next:
         slow = slow.next
         fast = fast.next.next
+        if slow == fast:
+            return slow
 
     return True
+    return None
+
